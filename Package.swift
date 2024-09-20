2201d4ee311be95baefc9ba90117cbce24cb8e27
László Kovács <atomantinator@gmail.com>
2024-09-20 10:31:29 +0200

    updated package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
         .package(url: "https://github.com/feather-framework/feather-openapi-kit", .upToNextMinor(from: "0.9.6")),
         .package(url: "https://github.com/feather-framework/feather-api-kit", .upToNextMinor(from: "0.1.1")),
         .package(url: "https://github.com/feather-modules/feather-system-api", .upToNextMinor(from: "0.2.0")),
-        .package(url: "https://github.com/feather-modules/feather-user-api", .upToNextMinor(from: "0.9.0")),
+        .package(url: "https://github.com/feather-modules/feather-user-api", branch: "fix/user-account-reference-image-key"),
+//        .package(url: "https://github.com/feather-modules/feather-user-api", .upToNextMinor(from: "0.9.0")),
         .package(url: "https://github.com/jpsim/Yams", from: "5.0.0"),
     ],
     targets: [
